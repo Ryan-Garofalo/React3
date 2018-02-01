9f8aeb9e47ea9b97a130fe42c7e44ba932ab0b7f
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-02-01 13:28:20 -0700

    chugging along

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = "f6be1a7f17576988becc95b3f6f4fd59";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+	const url =`${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	console.log(request + '++++++');
+
+
+	return {
+        type: FETCH_WEATHER,
+		payload:request
+    };
+}
